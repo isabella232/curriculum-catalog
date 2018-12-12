d95b9998a7761d43352875e620e0aa82fcffea70
Ryan Henning <ryan@rhobota.com>
2018-12-12 13:25:48 -0600

    Regen

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <v-layout column justify-center align-center>
     <v-flex xs12 sm8 md6>
       <div class="text-xs-center">
-        <img src="/v.png" alt="Vuetify.js" class="mb-5" />
+        <img src="https://cdn.vuetifyjs.com/images/logos/v-alt.svg" width=200 alt="Vuetify.js" class="mb-5" />
       </div>
       <v-card>
         <v-card-title class="headline">Welcome to the Vuetify + Nuxt.js template</v-card-title>
