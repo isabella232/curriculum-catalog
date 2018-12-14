017c9ca202d732ab27a62fce788b1f63a4f3d560
Ryan Henning <ryan@rhobota.com>
2018-12-14 00:22:30 -0600

    Test

--- layouts/default.vue
+++ layouts/default.vue
@@ -15,16 +15,6 @@
   export default {
     data () {
       return {
-        clipped: false,
-        drawer: true,
-        fixed: false,
-        items: [
-          { icon: 'apps', title: 'Welcome', to: '/' },
-          { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' }
-        ],
-        miniVariant: false,
-        right: true,
-        rightDrawer: false,
         title: 'Quansight Course Catalog'
       }
     }
